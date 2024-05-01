78450c1eff84d69ea1c60fafc5d4ad73c9950a90
wilsonjj3 <122841036+wilsonjj3@users.noreply.github.com>
2024-05-01 02:30:35 -0500

    deleting blood bank

--- BloodBank/backToFront.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<form id ='goBack' action='index.php' method='post' >";
-echo " <input type='submit' value='Return Home'> <br />";
-echo "</form>";
-?>
--- BloodBank/header.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<img src=\"mater.png\" alt=\"Italian Trulli\"> <h1>Truck Tracker Database</h1> ";
-?>
